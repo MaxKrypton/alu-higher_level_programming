27433bfaa4ed771440762ebcdf1db7adde565a98
Gentil Quentin <q.rurangirw@alustudent.com>
2024-02-23 17:27:19 +0200

    Quentin Default Commit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str)
-print(str[39:65], "with python")
+print(str[39:65], end=" ")
+print("with python")
