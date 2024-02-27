562223ca2ae901ad3b4e98d80d68511108a2fb12
Gentil Quentin <q.rurangirw@alustudent.com>
2024-02-27 23:06:00 +0200

    Quentin Second Default Commit

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for k in range(00, 100):
-    print("{:02d},".format(k), end=" ")
+    if k < 99:
+        print("{:02d},".format(k), end=" ")
+    if k == 99:
+        print("{:02d}".format(k), end="")
