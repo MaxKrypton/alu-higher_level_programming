cd4c49082dff31c77cc4e1947320230f5b9b9ee5
MaxKrypton <q.rurangirw@alustudent.com>
2024-03-17 23:02:19 +0200

    Quentin class Default Commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Create an Empty Square """
+# 0-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
+
+
 class Square:
-    ''' creates an empty square'''
+    """Represent a square."""
     pass
